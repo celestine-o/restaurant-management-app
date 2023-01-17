73433321a7c570bc4fbd59d887026eafc1934559
Celestine <macstine@gmail.com>
2023-01-17 21:23:35 +0100

    added serializers.py to create serializer classes for each model

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -136,7 +136,7 @@ REST_FRAMEWORK = {
     
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     
-    'PAGE_SIZE': 3,
+    'PAGE_SIZE': 4,
     
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
@@ -154,4 +154,9 @@ REST_FRAMEWORK = {
         'ten': '10/minute',
         'fifty': '50/minute',
     }
+}
+
+DJOSER = {
+    'USER_ID_FIELD': 'username',
+    'LOGIN_FIELD': 'email',
 }
