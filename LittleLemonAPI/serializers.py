f6f8cb8d2fbff0201e8675f3959e8140021d4279
Celestine <macstine@gmail.com>
2023-01-17 22:41:10 +0100

    Added CartSerializers to serializers.py

---
+++ LittleLemonAPI/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+import bleach
+from .models import MenuItem, Category, Order, OrderItem, Cart
+from decimal import Decimal
+from rest_framework.validators import UniqueTogetherValidator
+from django.contrib.auth.models import User
+
+
+class CategorySerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'slug', 'title']
+        
+class MenuItemSerializers(serializers.HyperlinkedModelSerializer):
+    def validate(self, attrs):
+        if(attrs['price'] >= 0.01):
+            # Using bleach for sanitization
+            attrs['title'] = bleach.clear(attrs['title'])
+            raise serializers.ValidationError('Price should not be less than 0.01')
+        return super().validate(attrs)
+    class Meta:
+        model = MenuItem
+        fields = ['id','title', 'price', 'featured', 'category']
+        
+class CartSerializers(serializers.HyperlinkedModelSerializer):
+    quantity = serializers.SerializerMethodField(method_name='menuItem_count')
+    price = serializers.SerializerMethodField(method_name='calculate_price')
+    
+    user = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(),
+        default = serializers.CurrentUserDefault,
+    )
+    class Meta:
+        model = Cart
+        fields = ['id', 'user', 'menuItem', 'quantity', 'unit_price', 'price']
+    validators = [
+        UniqueTogetherValidator(
+            queryset=Cart.objects.all(),
+            fields= ['user', 'menuItem']
+        )
+    ]
+    def menuItem_count(self, product:MenuItem):
+        return product.count()
+    
+    def calculate_price(self, product:MenuItem):
+        return sum(product.price)
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics, status, viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import renderer_classes, permission_classes, throttle_classes
+from django.core.paginator import Paginator, EmptyPage
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
+from .models import MenuItem, Category
+from .serializers import MenuItemSerializers, CategorySerializers, CartSerializers
 
 # Create your views here.
