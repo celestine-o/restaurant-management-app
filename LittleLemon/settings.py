c62fb31ccb27544bcc07b5c7b3fc4f650306bdc7
Celestine <macstine@gmail.com>
2023-01-17 05:38:01 +0100

    Added Category, MenuItem, Cart, Order, OrderItem models

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -136,7 +136,7 @@ REST_FRAMEWORK = {
     
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     
-    'PAGE_SIZE': 2,
+    'PAGE_SIZE': 3,
     
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
